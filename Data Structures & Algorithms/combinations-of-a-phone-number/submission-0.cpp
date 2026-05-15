df9369a47697f09be07ff251fd6df1d41fbd8cd3
Suraj Kumar Agrahari <150293632+2Surajagrahari@users.noreply.github.com>
2026-05-15 23:25:33 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> result;
+    void solve(int idx,string &tmp, string &digits, unordered_map<char,string> &mp){
+        //bs
+        if(idx >= digits.length()){
+            result.push_back(tmp);
+            return;
+        }
+
+        char ch = digits[idx];
+        string str = mp[ch];
+        for(int i=0;i<str.length();i++){
+            //do
+            tmp.push_back(str[i]);
+            solve(idx+1,tmp,digits,mp);
+            tmp.pop_back();
+        }
+    }
+     
+    vector<string> letterCombinations(string digits) {
+        if(digits.length() == 0){
+            return {};
+        }
+        unordered_map<char,string> mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+        string tmp = "";
+        solve(0,tmp,digits,mp);
+        return result;
+    }
+};
